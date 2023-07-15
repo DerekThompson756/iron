308b78a26fbc2b472d61e25a6096ffddd95d989d
Benjamin Belnap <ben.belnap@extendhealth.com>
2023-07-14 18:59:36 -0600

    add skill to accessory.py

--- app/data/accessory.py
+++ app/data/accessory.py
@@ -1,9 +1,10 @@
 from dataclasses import dataclass
 from item import Item
+from skill import Skill
 
 @dataclass
 class Accessory(Item):
     usable: bool
-    stats: dict
-    skill_on_equip: Skill
-    skill_on_use: Skill
+    stats: dict #(str; int)
+    skill_on_equip: Skill #nullable
+    skill_on_use: Skill #nullable
